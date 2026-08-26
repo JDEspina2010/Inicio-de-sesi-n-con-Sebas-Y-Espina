19909ebbd60a74fe227e55f4a18c6ed7d5fd540b
JDEspina <josuedaniel.espina@gmail.com>
2026-08-26 13:10:13 -0600

    codigo mejorado

--- codigos2.js
+++ codigos2.js
@@ -2,12 +2,12 @@ function reg() {
     let usu = document.getElementById("usuario").value;
     let cont = document.getElementById("cont").value;
     if (usu != "" && cont != "") {
-        document.getElementById("text").textContent = "Datos guardados"
+        document.getElementById("mensajes").textContent = "Datos guardados"
     } else if (usu == "" && cont == "") {
-        document.getElementById("text").textContent = "Llene todas las casillas por fabor"
+        document.getElementById("mensajes").textContent = "Llene todas las casillas por fabor"
     } else if (usu != "" && cont == "") {
-        document.getElementById("text").textContent = "Llene el espacio de contraseña"
+        document.getElementById("mensajes").textContent = "Llene el espacio de contraseña"
     } else if (usu == "" && cont != "") {
-        document.getElementById("text").textContent = "Llene el espacio de usuario"
+        document.getElementById("mensajes").textContent = "Llene el espacio de usuario"
     }
 }
