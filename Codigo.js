5d4ae6145be47ac22e87b68709c3042fa642d0ce
JDEspina <josuedaniel.espina@gmail.com>
2026-08-26 13:09:42 -0600

    mejoras

--- Codigo.js
+++ Codigo.js
@@ -1,18 +1,18 @@
 function inicio() {
-    let usuario = document.getElementById("user").value;
-    let contrasena = document.getElementById("contrasena").value;
+    let usuario = document.getElementById("exampleInputEmail1").value;
+    let contrasena = document.getElementById("exampleInputPassword1").value;
     if (usuario === "admin" && contrasena == "1234") {
-        document.getElementById("mensaje").textContent = "Inicio de sesión correcto"
+        document.getElementById("notificacion").textContent = "Inicio de sesión correcto"
     } else if (usuario == "alumno" && contrasena == "4321") {
-        document.getElementById("mensaje").textContent = "Inicio de sesión correcto"
+        document.getElementById("notificacion").textContent = "Inicio de sesión correcto"
     } else if (usuario == "" && contrasena == "") {
-        document.getElementById("mensaje").textContent = "Llene todas las casillas por fabor"
+        document.getElementById("notificacion").textContent = "Llene todas las casillas por fabor"
     } else if (usuario != "" && contrasena == "") {
-        document.getElementById("mensaje").textContent = "Llene el espacio de contraseña"
+        document.getElementById("notificacion").textContent = "Llene el espacio de contraseña"
     } else if (usuario == "" && contrasena != "") {
-        document.getElementById("mensaje").textContent = "Llene el espacio de usuario"
+        document.getElementById("notificacion").textContent = "Llene el espacio de usuario"
     } else {
-        document.getElementById("mensaje").textContent = "Usuario y/o contraseña incorrecto"
+        document.getElementById("notificacion").textContent = "Usuario y/o contraseña incorrecto"
     }
 }
 
