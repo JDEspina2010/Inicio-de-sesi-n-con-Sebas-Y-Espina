a1a75468f6771339c2dab67139716b4bdf319219
skinny-sebs <sebastilin29@gmail.com>
2026-08-26 13:10:04 -0600

    CODIGOS CHILEROS

--- codigos2.js
+++ codigos2.js
@@ -1,13 +1,13 @@
 function reg() {
-    let usu = document.getElementById("usuario").value;
-    let cont = document.getElementById("cont").value;
+    let usu = document.getElementById("exampleInputEmail1").value;
+    let cont = document.getElementById("exampleInputPassword1").value;
     if (usu != "" && cont != "") {
-        document.getElementById("text").textContent = "Datos guardados"
+        document.getElementById("mensajes").textContent = "Datos guardados"
     } else if (usu == "" && cont == "") {
-        document.getElementById("text").textContent = "Llene todas las casillas por fabor"
+        document.getElementById("mensajes").textContent = "Llene todas las casillas por fabor"
     } else if (usu != "" && cont == "") {
-        document.getElementById("text").textContent = "Llene el espacio de contraseña"
+        document.getElementById("mensajes").textContent = "Llene el espacio de contraseña"
     } else if (usu == "" && cont != "") {
-        document.getElementById("text").textContent = "Llene el espacio de usuario"
+        document.getElementById("mensajes").textContent = "Llene el espacio de usuario"
     }
 }
